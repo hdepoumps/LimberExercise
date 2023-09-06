8d209e564683e6aefeff9cf00190ecef1bbbb167
Hugo Depoumps <depoumpshugo@gmail.com>
2023-09-06 19:39:11 +0200

    add logo and icons not on fontawesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,14 @@
-export default class Header {
+import logo from '../img/limber-logo-114.png'
 
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt={"logo Limber"}/>
+            <nav>
+
+            </nav>
+        </header>
+    )
 }
+
+export default Header
