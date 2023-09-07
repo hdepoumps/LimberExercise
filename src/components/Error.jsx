b8082a76fd6cd76aeb16fa62bca010875c91acad
Hugo Depoumps <depoumpshugo@gmail.com>
2023-09-07 22:24:06 +0200

    creation of the first 2 sections

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,3 +1,12 @@
-export default class Error {
-
+import { Link } from 'react-router-dom'
+function Error() {
+    return (
+        <div className={"error"}>
+            {/*<h1>404</h1>
+            <h2 className={""}>Oups! La page que vous demandez n'existe pas.</h2>
+            <Link to="/">Retourner sur la page d’accueil</Link>*/}
+        </div>
+    )
 }
+
+export default Error
--- src/pages/Campagnes.jsx
+++ src/pages/Campagnes.jsx
@@ -1,3 +1,64 @@
-export default class Campagnes {
+import '../style/campagnes.css'
+import campagnedata from '../data/campagne.json'
+import Campagne from '../components/campagne'
+import notifIcon from '../img/notification-icon.svg'
+import profil from '../img/profilePicture.png'
+import arrow from '../img/fleche-icon.svg'
+import search from '../img/search-icon.svg'
+function Campagnes(){
+    const campagneData =campagnedata.map((item,index) => {
+        return(
+                <Campagne
+                    key={index}
+                    nom={item.nom}
+                    type={item.type}
+                    partage={item.nombre_de_partage}
+                    attente={item.nombre_de_partage_en_attente}
+                    clic={item.nombre_de_clic}
+                    contact={item.nombre_de_conntact}
+                />
+            )
+    })
+    return(
+        <main>
+            <section className={"entete"}>
+                <h2>Campagnes</h2>
+                <div className={"compte"}>
+                    <img src={notifIcon} alt={"icone de notification"}/>
+                    <p>Jeremy</p>
+                    <img src={profil} alt={"photo de profil"}/>
+                    <img src={arrow} alt={"icone de flèche"}/>
+                </div>
+            </section>
+            <section className={"filtre"}>
+                <div className={"gestioncampagne"}>
+                    <p>Campagnes</p>
+                    <p>Mots-clés</p>
+                    <button type="button">Nouvelle campagne</button>
+                </div>
+                <div className={"recherche"}>
+                    <select>
+                        <option value={0}>Date de création. desc.</option>
+                        <option value={1}>Date de création. asc.</option>
+                        <option value={2}>Date de modification. desc.</option>
+                        <option value={3}>Date de modification. asc.</option>
+                    </select>
+                    <div>
+                        <input type="search" name="q" placeholder="Recherche..." />
+                        <img className={"search"} src={search} alt={"loupe"}/>
+                    </div>
+                </div>
+            </section>
+            <section className={"campagnes"}>
+                <div className={"legend"}>
+                    <p>Nom</p>
+                    <p>Partages</p>
+                    <p>Clics</p>
+                    <p>Contacts</p>
+                </div>
 
+            </section>
+        </main>
+    )
 }
+export default Campagnes
